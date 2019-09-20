55d065f16c7c22afe64be96ff6deea20659ac624
unknown <amit14nov@yahoo.com>
2019-09-20 11:13:12 +0100

    changes made to check git push

--- src/main/java/com/hotelApp/qa/base/TestBase.java
+++ src/main/java/com/hotelApp/qa/base/TestBase.java
@@ -46,6 +46,7 @@ public class TestBase
        driver.manage().timeouts().pageLoadTimeout(UtilCommon.waitPageTimeOut,TimeUnit.SECONDS);
        driver.manage().timeouts().implicitlyWait(UtilCommon.implicitWaitTimeOut, TimeUnit.SECONDS);
        driver.get(prop.getProperty("url"));
+       System.out.println("Testing Git push");
    }
 
 }
