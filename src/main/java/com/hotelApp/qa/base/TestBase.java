a5db7f0ccdd3b15a7c28753c9b5f2c175afef9d0
kumaramit-bhabra <Loadrunner_1>
2019-11-29 13:41:42 +0000

    Changes to Test Base

--- src/main/java/com/hotelApp/qa/base/TestBase.java
+++ src/main/java/com/hotelApp/qa/base/TestBase.java
@@ -44,7 +44,7 @@ public class TestBase
 //Changes made
        if(browserName.equals("Chrome"))
        {
-           System.setProperty("webdriver.chrome.driver","src//main//java//com//hotelApp//qa//testData//chromedriver.exe");
+           System.setProperty("webdriver.chrome.driver","src//main//java//com//hotelApp//qa//testData//chromedriver");
            //System.setProperty("webdriver.chrome.driver","usr//local//bin//chromedriver");
            driver = new ChromeDriver();
            /*DesiredCapabilities cap = new DesiredCapabilities();
